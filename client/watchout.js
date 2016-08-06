d7bccee42f71e6d107cbdd9f24d1047dfbc78c20
brandontiqui <btiqui@gmail.com>
2016-08-06 10:22:54 -0700

    Clear console error

--- client/watchout.js
+++ client/watchout.js
@@ -2,7 +2,7 @@
 var gameOptions = {
   height: 450,
   width: 700,
-  nEnemies: 12, // testing
+  nEnemies: 30, // testing
   padding: 20
 };
 
@@ -32,8 +32,7 @@ var player = function() {
 }; 
 
 // init circle position
-var x = 100; 
-var y = 20; 
+ 
 
 var drag = d3.behavior.drag()
 .on('drag', function(d, i) {
@@ -71,7 +70,7 @@ var render = function(enemyData) {
   .attr('cy', function(enemy) {
     return axes.y(enemy.y);
   })
-  .attr('r', 15)
+  .attr('r', 5)
   .style('fill', 'black')
   .transition().duration(2000);
 
@@ -98,8 +97,8 @@ var postRender = function(enemyData) {
     return axes.y(enemy.y);
   })
   .attr('r', 15)
-  .style('fill', 'black')
-  .call(drag);
+  .style('fill', 'black');
+  // .call(drag);
   
 
   enemies.exit().remove(); 
@@ -113,97 +112,15 @@ setInterval(function() {
   postRender(createEnemies());
 }, 2000); 
 
-// var alertt = function() {
-//   // console.log('in');
-//   var enemies;
-//   setInterval(function() {
-//     // render(createEnemies());
-//     // call createEnemies to get new positions
-//     // enemies = createEnemies();
-//     // console.log(enemies);
-//     // render(enemies);
-//     // call render with new positions
-//   }, 3000);
-// };
-// alertt();
-
-// var newEnemies = render(createEnemies()); 
-// newEnemies(); 
-// setInterval(newEnemies, 1000);
-
-
-// var svgContainer = d3.select('body').append('svg')
-//   .attr('width', 1000)
-//   .attr('height', 1000);
-
-// //Draw the Circle
-
+var x = 100; 
+var y = 20;
 var circle = gameBoard.selectAll('circle.player')
   // .attr('cx', x)
   // .attr('cy', y)
-  .data([ {'x': 100, 'y': 100} ])
+  .data([ {'x': x, 'y': y} ])
   .enter()
   .append('svg:circle') // try
   .attr('transform', 'translate(' + x + ',' + y + ')')
   .attr('r', 20)
   .style('fill', 'blue')
   .call(drag);
-
-
-//   var render = function(enemyData) {
-//   var enemies = gameBoard.selectAll('circle.enemy')
-//   .data(enemyData, function(d) { return d.id; }); 
-
-//   enemies.enter()
-//   .append('svg:circle')
-//   .attr('class', 'enemy')
-//   .attr('cx', function(enemy) {
-//     return axes.x(enemy.x);
-//   })
-//   .attr('cy', function(enemy) {
-//     return axes.y(enemy.y);
-//   })
-//   .attr('r', 15)
-//   .style('fill', 'black')
-//   .transition().duration(2000);
-
-//   enemies.exit().remove(); 
-
-// }; 
-
-
-// d3.select('body')
-//   .selectAll('svg')
-//   .data(data)
-//   .enter()
-//   .append('svg')
-//   .text(function(d) { return d; });
-
-
-
-// circleRadii = [90, 50, 70, 40, 20, 10, 5, 4];
-
-// var svgContainer = d3.select('body').append('svg')
-//   .attr('width', 1000)
-//   .attr('height', 1000);
-
-// var circles = svgContainer.selectAll('circle')
-//   .data(circleRadii)
-//   .enter()
-//   .append('circle');
-
-// var circleAttributes = circles
-//   .attr('cx', 100)
-//   .attr('cy', 100)
-//   .attr('r', function (d) { return d; })
-//   .style('fill', function(d) {
-//     var returnColor;
-//     if (d === 40) { 
-//       returnColor = 'green'; 
-//     } else if (d === 20) { 
-//       returnColor = 'purple';
-//     } else if (d === 10) { 
-//       returnColor = 'red'; 
-//     }
-//     return returnColor;
-//   });
